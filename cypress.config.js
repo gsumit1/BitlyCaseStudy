1ff1988829c800fc6d87fe5914e2d7db9a5e6db5
Sumit Ghosh <sumit.ghosh@experian.com>
2022-12-15 16:25:22 +0530

    Added files

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  includeShadowDom: true,
+  chromeWebSecurity: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://www.qrcode-monkey.com',
+  },
+}
